00e8a45a861e22952c981ade3dabc1f82a0d40eb
Rafał Tupacz <r.tupacz@gmail.com>
2020-01-25 20:47:18 +0100

    tree generator

--- index.js
+++ index.js
@@ -1,123 +1,138 @@
 const canvas = document.getElementById('canvas');
-canvas.width = 1200;
-canvas.height = 900;
+canvas.width = window.innerWidth;
+canvas.height = window.innerWidth;
 const ctx = canvas.getContext('2d');
 
 const playerImg = document.getElementById('playerImg');
-const treeImg = document.getElementById('treeImg');
-const bulletImg = document.getElementById('bulletImg');
 
-const player = {
-	name: 'Yerdek',
-	alive: true,
-	ammo: 5,
-	width: 100,
-	height: 70,
-	posX: 100,
-	posY: 200,
-	speed: 1,
-	dirX: 0,
-	dirY: 0,
+let playersAlive = [];
+let treesArr = [];
+let bulletsArr = [];
+
+let options = {
+    treeNumber: 7,
 };
 
-const playersAlive = [];
-const trees = [];
-const bullets = [];
-
-const bullet = {
-	width: 50,
-	height: 50,
-	posX: 0, // (player.posX + player.width - 15),
-	posY: 0, // (player.posY + player.height - 21),
-	dirX: 0,
-	dirY: 0,
-	speed: 15,
+const player = {
+    name: 'Yerdek',
+    alive: true,
+    ammo: 5,
+    width: 100,
+    height: 70,
+    posX: 100,
+    posY: 200,
+    speed: 1,
+    dirX: 0,
+    dirY: 0,
 };
 
 const tree = {
-	width: 400,
-	height: 400,
-	posX: Math.floor(Math.random() * (canvas.width - 200 - 0 + 1)) + 0,
-	posY: Math.floor(Math.random() * (canvas.height - 200 - 0 + 1)) + 0,
-	dirX: 0,
-	dirY: 0,
-	speed: 0,
+    width: 400,
+    height: 400,
+    posX: Math.floor(Math.random() * (canvas.width - 200 - 0 + 1)) + 0,
+    posY: Math.floor(Math.random() * (canvas.height - 200 - 0 + 1)) + 0,
+    dirX: 0,
+    dirY: 0,
+    speed: 0,
 };
 
 let = isRunning = false;
 
 function clearScreen() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
 function drawGui() {
-	ctx.font = '20px Arial';
-	ctx.fillStyle = 'green';
-	ctx.fillText('Player ' + player.name + ' is ' + (player.alive ? 'alive' : 'dead'), 40, 40, 150);
-	ctx.fillText('Ammo: ' + player.ammo, 40, 65, 150), ctx.fillText('Running:  ' + (isRunning ? 'On' : 'No'), 40, 90, 150);
+    ctx.font = '20px Arial';
+    ctx.fillStyle = 'green';
+    ctx.fillText('Player ' + player.name + ' is ' + (player.alive ? 'alive' : 'dead'), 40, 40, 150);
+    ctx.fillText('Ammo: ' + player.ammo, 40, 65, 150), ctx.fillText('Running:  ' + (isRunning ? 'On' : 'No'), 40, 90, 150);
+
+    ctx.font = '10px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText(player.name, player.posX + 20, player.posY, 70);
 }
 
 function drawPlayer() {
-	ctx.drawImage(playerImg, player.posX, player.posY, player.width, player.height);
+    ctx.drawImage(playerImg, player.posX, player.posY, player.width, player.height);
 }
 
 function drawTree() {
-	ctx.drawImage(treeImg, tree.posX, tree.posY, tree.width, tree.height);
+    for (let i = 0; i < treesArr.length; i++) {
+        ctx.drawImage(treesArr[i].texture, treesArr[i].posX, treesArr[i].posY), treesArr[i].width, treesArr[i].height;
+    }
 }
 
-function drawBullet() {
-    ctx.drawImage(bulletImg, (player.posX + player.width - 15), (player.posY + player.height - 21), bullet.width, bullet.height);
+function createTree() {
+    for (let i = 0; i < options.treeNumber; i++) {
+        let tree = new Tree();
+        treesArr.push(tree);
+    }
+}
+
+function createBullet() {
+    if (bulletsArr.length == 0) {
+        console.log('ODŁAMKOWYM!');
+        let bullet1 = new Bullet();
+        bulletsArr.push(bullet1);
+    } else {
+        console.log('JEST JUŻ KULKA');
+    }
 }
 
 function newPossition() {
-	player.posX += player.dirX;
-	player.posY += player.dirY;
-}
-
-function fireBullet() {
-	bullet.posX += bullet.dirX;
+    player.posX += player.dirX;
+    player.posY += player.dirY;
 }
 
 function update() {
-	clearScreen();
-	drawPlayer();
+    clearScreen();
+    drawPlayer();
     drawTree();
-    drawBullet(); 
-	drawGui();
-	fireBullet();
-	newPossition();
-	wallDetection();
-	requestAnimationFrame(update);
+    drawGui();
+    newPossition();
+    wallDetection();
+    requestAnimationFrame(update);
 }
 
 function wallDetection() {
-	player.posX < 0 ? (player.posX = 0) : null;
-	player.posY < 0 ? (player.posY = 0) : null;
-	player.posX + player.width > canvas.width ? (player.posX = canvas.width - player.width) : null;
-	player.posY + player.height > canvas.height ? (player.posY = canvas.height - player.height) : null;
+    player.posX < 0 ? (player.posX = 0) : null;
+    player.posY < 0 ? (player.posY = 0) : null;
+    player.posX + player.width > canvas.width ? (player.posX = canvas.width - player.width) : null;
+    player.posY + player.height > canvas.height ? (player.posY = canvas.height - player.height) : null;
 }
 
 function keyDownEvent(e) {
-	playerIsRunning(e);
+    console.log('KEY: ', e.key);
+    playerIsRunning(e);
 
-	e.keyCode == 32 ? (bullet.dirX = 1 * bullet.speed) : null;
-	e.key === 'd' ? (player.dirX = 1 * player.speed) : null;
-	e.key === 'a' ? (player.dirX = -1 * player.speed) : null;
-	e.key === 'w' ? (player.dirY = -1 * player.speed) : null;
-	e.key === 's' ? (player.dirY = 1 * player.speed) : null;
+    e.key === 'q' ? createBullet() : null;
+    e.key === 'e' ? createTree() : null;
+    e.key === 'z' ? gunFire() : null;
+    e.key === 'd' ? (player.dirX = 1 * player.speed) : null;
+    e.key === 'a' ? (player.dirX = -1 * player.speed) : null;
+    e.key === 'w' ? (player.dirY = -1 * player.speed) : null;
+    e.key === 's' ? (player.dirY = 1 * player.speed) : null;
 }
 
 function keyUpEvent(e) {
-	e.key === 'd' ? (player.dirX = 0) : null;
-	e.key === 'a' ? (player.dirX = 0) : null;
-	e.key === 'w' ? (player.dirY = 0) : null;
-	e.key === 's' ? (player.dirY = 0) : null;
+    e.key === 'd' ? (player.dirX = 0) : null;
+    e.key === 'a' ? (player.dirX = 0) : null;
+    e.key === 'w' ? (player.dirY = 0) : null;
+    e.key === 's' ? (player.dirY = 0) : null;
 }
 
 function playerIsRunning(e) {
-	e.key === 'Shift' ? (isRunning = !isRunning) : null;
-	isRunning ? (player.speed = 5) : (player.speed = 1);
+    e.key === 'Shift' ? (isRunning = !isRunning) : null;
+    isRunning ? (player.speed = 5) : (player.speed = 1);
 }
 
+function gunFire() {
+    bulletsArr = [];
+    console.log('Fire: ', bulletsArr);
+}
+
+createTree();
 update();
 
 document.addEventListener('keydown', keyDownEvent);
--- js/models.js
+++ js/models.js
@@ -1,5 +1,6 @@
 class Player {
-    constructor(name, alive, ammo, width, height, posX, posY, speed, dirX, dirY) {
+    constructor(texture, name, alive, ammo, width, height, posX, posY, speed, dirX, dirY) {
+        this.texture = texture;
         this.name = name;
         this.alive = alive;
         this.ammo = ammo;
@@ -14,25 +15,24 @@ class Player {
 }
 
 class Tree {
-    constructor(width, height, posX, posY, dirX, dirY, speed) {
-        this.width = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
-        this.height = Math.floor(Math.random() * (400 - 200 + 1)) + 200;
+    constructor(texture, width, height, posX, posY) {
+        this.texture = document.getElementById('treeImg');
+        this.width = Math.floor(Math.random() * (600 - 400 + 1)) + 400;
+        this.height = Math.floor(Math.random() * (600 - 400 + 1)) + 400;
         this.posX = Math.floor(Math.random() * (canvas.width - 200 - 0 + 1)) + 0;
         this.posY = Math.floor(Math.random() * (canvas.height - 200 - 0 + 1)) + 0;
-        this.dirX = 0;
-        this.dirY = 0;
-        this.speed = 0;
     }
 }
 
 class Bullet {
-    constructor(width, height, posX, posX, dirX, dirY, speed) {
+    constructor(texture, width, height, posX, posY, dirX, dirY, speed) {
+        this.texture = document.getElementById('bulletImg');
         this.width = 10;
         this.height = 5;
-        this.posX = posY;
-        this.posY = posY;
-        this.dirX = dirx;
-        this.dirY = dirY;
-        this.speed = speed;
+        this.posX = player.posX + player.width - 15;
+        this.posY = player.posY + player.height - 21;
+        this.dirX = 0;
+        this.dirY = 0;
+        this.speed = 15;
     }
 }
